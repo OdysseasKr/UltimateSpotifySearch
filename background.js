fac659c8e1327afd18cf6f678d2adb186bb70663
OdysseasKr <OdysseasKr@users.noreply.github.com>
2017-12-01 13:35:37 +0200

    Convert to tab indents

--- background.js
+++ background.js
@@ -1,56 +1,56 @@
 /*
-    This file is part of Ultimate Spotify Search.
+	This file is part of Ultimate Spotify Search.
 
-    Ultimate Spotify Search is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	Ultimate Spotify Search is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    Ultimate Spotify Search is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Ultimate Spotify Search is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
 */
 // On install
 chrome.runtime.onInstalled.addListener(function () {
-    // Opens the first run page
-    chrome.tabs.create({url: "extensionPages/firstRun.html"});
-    
-    // Adds the context menu item
-    chrome.contextMenus.create({
-        id: "spotisearchcontext",
-        title: "Search on Spotify",
-        contexts: ["selection"]
-    });
-    
-    // Sets the default setting for the search target
-    chrome.storage.sync.set({"ultimateSpotifyButton": 1});
+	// Opens the first run page
+	chrome.tabs.create({url: "extensionPages/firstRun.html"});
+
+	// Adds the context menu item
+	chrome.contextMenus.create({
+		id: "spotisearchcontext",
+		title: "Search on Spotify",
+		contexts: ["selection"]
+	});
+
+	// Sets the default setting for the search target
+	chrome.storage.sync.set({"ultimateSpotifyButton": 1});
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    chrome.storage.sync.get("ultimateSpotifyButton", function (result) {
-        if (result.ultimateSpotifyButton == 1) {
-            chrome.tabs.update(tab.id, {url: "spotify:search:\"" + info.selectionText + "\""});
-        } else {
-            chrome.tabs.create({url: "https://play.spotify.com/search/" + info.selectionText});
-        }
-    });
+	chrome.storage.sync.get("ultimateSpotifyButton", function (result) {
+		if (result.ultimateSpotifyButton == 1) {
+			chrome.tabs.update(tab.id, {url: "spotify:search:\"" + info.selectionText + "\""});
+		} else {
+			chrome.tabs.create({url: "https://play.spotify.com/search/" + info.selectionText});
+		}
+	});
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // If the message is send by the settings page
-    if (request.target >= 0) {
-        chrome.storage.sync.set({"ultimateSpotifyButton": request.target});
-    } else { // If the message is send by a content script
-        chrome.storage.sync.get("ultimateSpotifyButton", function (result) {
-            if (result.ultimateSpotifyButton == 1) {
-                chrome.tabs.update(sender.tab.id, {url: "spotify:search:\"" + request.terms + "\""});
-            } else {
-                chrome.tabs.create({url: "https://play.spotify.com/search/" + request.terms});
-            }
-        });
-    }
+	// If the message is send by the settings page
+	if (request.target >= 0) {
+		chrome.storage.sync.set({"ultimateSpotifyButton": request.target});
+	} else { // If the message is send by a content script
+		chrome.storage.sync.get("ultimateSpotifyButton", function (result) {
+			if (result.ultimateSpotifyButton == 1) {
+				chrome.tabs.update(sender.tab.id, {url: "spotify:search:\"" + request.terms + "\""});
+			} else {
+				chrome.tabs.create({url: "https://play.spotify.com/search/" + request.terms});
+			}
+		});
+	}
 });
--- extensionPages/settings.js
+++ extensionPages/settings.js
@@ -1,47 +1,47 @@
 /*
-    This file is part of Ultimate Spotify Search.
+	This file is part of Ultimate Spotify Search.
 
-    Ultimate Spotify Search is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	Ultimate Spotify Search is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    Ultimate Spotify Search is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Ultimate Spotify Search is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
 */
 // Load values
 chrome.storage.sync.get("ultimateSpotifyButton", function (result) {
-    if (result.ultimateSpotifyButton == 1) {
-        document.getElementById("desktopTarget").checked = true;
-    } else {
-        document.getElementById("webTarget").checked = true;
-    }
+	if (result.ultimateSpotifyButton == 1) {
+		document.getElementById("desktopTarget").checked = true;
+	} else {
+		document.getElementById("webTarget").checked = true;
+	}
 });
 
 // Save changes on button click
 document.getElementById("saveButton").addEventListener("click", function () {
-    if (document.getElementById("desktopTarget").checked) {
-        chrome.runtime.sendMessage({target: 1});
-    } else {
-        chrome.runtime.sendMessage({target: 0});
-    }
-    
-    blinkMessage(document.getElementById("notifier"));
+	if (document.getElementById("desktopTarget").checked) {
+		chrome.runtime.sendMessage({target: 1});
+	} else {
+		chrome.runtime.sendMessage({target: 0});
+	}
+
+	blinkMessage(document.getElementById("notifier"));
 });
 
 function blinkMessage(element) {
-    var opac = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (opac <= 0.1) {
-            clearInterval(timer);
-            element.style.opacity = 1;
-        }
-        element.style.opacity = opac;
-        opac -= opac * 0.1;
-    }, 50);
+	var opac = 1;  // initial opacity
+	var timer = setInterval(function () {
+		if (opac <= 0.1) {
+			clearInterval(timer);
+			element.style.opacity = 1;
+		}
+		element.style.opacity = opac;
+		opac -= opac * 0.1;
+	}, 50);
 }
