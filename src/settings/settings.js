5f4102e00586050e6721b86b50e2e897b34834ae
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-08-09 01:27:13 +0200

    Simplify settings

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -15,33 +15,12 @@
 	along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
 */
 // Load values
-chrome.storage.local.get("ultimateSpotifyButton", function (result) {
-	if (result.ultimateSpotifyButton == 1) {
-		document.getElementById("desktopTarget").checked = true;
-	} else {
-		document.getElementById("webTarget").checked = true;
-	}
+chrome.storage.local.get("ultimateSpotifyButton", (result) => {
+  document.querySelector("#spotifyTarget").value = result.ultimateSpotifyButton.toString();
 });
 
 // Save changes on button click
-document.getElementById("saveButton").addEventListener("click", function () {
-	if (document.getElementById("desktopTarget").checked) {
-		chrome.runtime.sendMessage({target: 1});
-	} else {
-		chrome.runtime.sendMessage({target: 0});
-	}
-
-	blinkMessage(document.getElementById("notifier"));
+document.querySelector("#spotifyTarget").addEventListener("change", (e) => {
+  chrome.storage.local.set({"ultimateSpotifyButton": parseInt(e.target.value)});
 });
 
-function blinkMessage(element) {
-	var opac = 1;  // initial opacity
-	var timer = setInterval(function () {
-		if (opac <= 0.1) {
-			clearInterval(timer);
-			element.style.opacity = 1;
-		}
-		element.style.opacity = opac;
-		opac -= opac * 0.1;
-	}, 50);
-}
