073befcfbc00e9e9386d6932f5e1dbc2b0644531
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-08-09 13:00:54 +0200

    Add button for Youtube

--- src/js/youtube.js
+++ src/js/youtube.js
@@ -15,124 +15,52 @@
 	along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
 */
 // Check which design is used
-if ($("#page-container").length != 0) { // Previous Design
-	setTimeout(function () {
-		injectYTOldDesign();
-	},1000);
-} else { // Material Design
-	setTimeout(function () {
-		injectYTMaterialDesign();
-		document.addEventListener('transitionend', function(e) {
-		if (e.target.id === 'progress')
-			setTimeout(function () {
-				injectYTMaterialDesign();
-			},1000);
-		});
-	}, 2000);
+console.log("HERE I AM")
+setTimeout(function () {
+  injectButton();
+  document.addEventListener('transitionend', function(e) {
+    if (e.target.id === 'progress')
+      setTimeout(function () {
+        injectButton();
+      },1000);
+  });
+}, 2000);
+
+
+const createButton = (text) => {
+	const element = $("<div></div>", {
+		text: "Spotify Search",
+		class: "spotifyButton",
+	});
+	element.css({
+		"cursor": "pointer",
+		"display": "inline-block",
+		"font-size": "1.4rem",
+		"border": "0.4rem solid #8dbe00",
+		"border-radius": "18px",
+		"padding": "0.15rem",
+    "background-color": "#8dbe00",
+    "position": "absolute",
+    "right": "0",
+    "top": "0",
+	});
+
+  element.on("click", function () {
+    openURI(filterText(text));
+  });
+	return element;
 }
 
-/*
- * Adds the button on youtube
- */
+function injectButton() {
+	if ($(".watch-active-metadata #title").length == 0)
+	  return;
+  if ($(".spotifyButton").length != 0)
+    return;
 
-function injectYTOldDesign() {
-	if ($("#watch-header").length == 0 ||
-		$(".spotifyButton").length != 0)
-		return;
-	// Button creation, styling and positioning
-	var spotifyButton = $("<div></div>", {
-		"class": "yt-uix-menu spotifyButton",
-	});
-	spotifyButton.css({
-		"border": "1px solid #8dbe00",
-		"borderRadius": "5px"
-	});
-
-	var firstDiv = $("<div></div>", {
-		"class": "yt-uix-menu-trigger"
-	});
-
-	var mainButton = $("<button></button>", {
-		"class": "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity",
-		"type": "button",
-		"id": "action-panel-overflow-button",
-		"aria-haspopup": "false",
-		"data-button-toggle": "false"
-	});
-
-	var textSpan = $("<span></span>", {
-		"class": "yt-uix-button-content",
-		text: "Search on Spotify"
-	});
-
-	mainButton.append(textSpan);
-	firstDiv.append(mainButton);
-	spotifyButton.append(firstDiv);
-	$("#watch8-secondary-actions").append(spotifyButton);
-
-	// Set the onclick attribute
-	mainButton.on("click", function () {
-		var title = $("#eow-title").text(),
-			cleanTitle = filterText(title);
-
-		openURI(cleanTitle);
-	});
-}
-
-function injectYTMaterialDesign() {
-	if ($("#top-level-buttons").length == 0 ||
-		$(".spotifyButton").length != 0)
-		return;
-	// Button creation, styling and positioning
-	var spotifyButton = $("<ytd-button-renderer></ytd-button-renderer>", {
-		class: "spotifyButton style-scope ytd-menu-renderer style-default",
-		"button-renderer": "",
-		"has-no-text": "",
-		"is-icon-button": "",
-	});
-	spotifyButton.css({
-		"border": "1px solid #8dbe00",
-		"border-radius": "5px"
-	});
-
-	var anchor = $("<a></a>", {
-		"is": "yt-endpoint",
-		"tabindex": "-1",
-		"class": "style-scope ytd-button-renderer"
-	});
-
-	var subButton = $("<button></button>", {
-		class: "style-scope yt-icon-button"
-	});
-	subButton.css({
-		"width": "100%",
-		"box-sizing": "border-box",
-		"padding": "20%",
-		"is": "paper-icon-button-light",
-		"aria-label": "Search on Spotify"
-	});
-
-
-	var icon = $("<img>", {
-		"src": chrome.runtime.getURL("icons/icon32-material.png")
-	})
-	icon.css("width", "100%");
+  spotifyButton = createButton($(".watch-active-metadata #title:first").text())
 	try{
-		$("#info #menu-container .ytd-video-primary-info-renderer #top-level-buttons").append(spotifyButton);
-		setTimeout(function (){
-			spotifyButton.append(anchor);
-			anchor.append(subButton);
-			subButton.append(icon)
-		},500);
-
-		// Set the onclick attribute
-
-		spotifyButton.on("click", function () {
-			var title = $(".title:first").text(),
-				cleanTitle = filterText(title);
-
-			openURI(cleanTitle);
-		});
+		$(".watch-active-metadata #title").append(spotifyButton);
 	} catch(e) {
+    console.log("Error injecting spotify button", e)
 	}
 }
