774dda62794c79dad310de434502e5d947a443ea
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-08-10 17:33:40 +0200

    Fix button not being add on new soundcloud tracks

--- src/js/soundcloud.js
+++ src/js/soundcloud.js
@@ -14,7 +14,7 @@
 	You should have received a copy of the GNU General Public License
 	along with Ultimate Spotify Search.  If not, see <http://www.gnu.org/licenses/>.
 */
-const soundcloudAdded = false;
+let soundcloudAdded = false;
 
 setTimeout(()=>{
   soundcloudButton();
